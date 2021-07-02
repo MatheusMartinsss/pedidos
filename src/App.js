d9a11fa49c61a9519e02ca1aedfd6fa9c8473c8a
MatheusMartinsss <matheus.s.martins98@gmail.com>
2021-07-01 22:55:17 -0400

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import Header from './Componentes/header'
+import Produtos from './Componentes/produtos'
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const Categorias = [{
+  Nome: 'Açai no copo 290ml',
+  Cod: 0
+}, {
+  Nome: 'Açai no copo 300ml',
+  Cod: 1,
+}, {
+  Nome: 'Açai pote 500ml',
+  Cod: 2,
+}, {
+  Nome: 'Barca de açai',
+  Cod: 3
+},{
+  Nome: 'Pote de Sorvete 500ml',
+  Cod: 4,
+},{
+  Nome: 'Pote de sorvete 1lt',
+  Cod: 5
+}];
+
+const Prods = [{
+  Nome: 'Açai no Copo 290ml',
+  Descricao: 'Acai com chocoball e confete',
+  Categoria: 0,
+  Preco: 10.00
+},{
+  Nome: 'Açai no Copo 290ml',
+  Descricao: 'Acai com chocoball e confete',
+  Categoria: 0,
+  Preco: 10.00
+},{
+  Nome: 'Açai no Copo 300ml',
+  Descricao: 'Acai com chocoball e confete',
+  Categoria: 1,
+  Preco: 15.00
+},{
+  Nome: 'Açai no Copo 300ml',
+  Descricao: 'Acai com chocoball e confete',
+  Categoria: 1,
+  Preco: 15.00
+},{
+  Nome: 'Açai no pote 500ML',
+  Descricao: 'Acai com chocoball e confete',
+  Categoria: 2,
+  Preco: 20.00
+},{
+  Nome: 'Açai no pote 500ML',
+  Descricao: 'Acai com Confete',
+  Categoria: 2,
+  Preco: 20.00
+},{
+  Nome: 'Barca de Acai',
+  Descricao: 'Acai com chocoball e confete',
+  Categoria: 3,
+  Preco: 10.00
+},{
+  Nome: 'Açai no Copo 290ml',
+  Descricao: 'Acai com chocoball e confete',
+  Categoria: 3,
+  Preco: 10.00
+},]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Container maxWidth='md'>
+        <Produtos Categorias={Categorias} Prods = {Prods} />
+      </Container>
     </div>
   );
 }
---
+++ src/Componentes/produtos.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`scrollable-auto-tabpanel-${index}`}
+            aria-labelledby={`scrollable-auto-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `scrollable-auto-tab-${index}`,
+        'aria-controls': `scrollable-auto-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+function Produtos(props) {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const Categorias = props.Categorias;
+    const Prods = props.Prods;
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        console.log(newValue)
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="scrollable"
+                    scrollButtons="auto"
+                    aria-label="scrollable auto tabs example"
+                >
+                    {Categorias.map((item) => (
+                        <Tab label={item.Nome} {...a11yProps(item.Cod)} />
+                    ))}
+
+                </Tabs>
+            </AppBar>
+            {Prods.filter(item => item.Categoria === value).map(item => (
+                <TabPanel value={item.Categoria} index={item.Categoria}>
+                    {item.Nome}
+                </TabPanel>
+            ))}
+
+
+        </div>
+    );
+}
+export default Produtos;
